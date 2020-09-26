3d2a5d423a843c8ac61b3eba68db8860fc1cc95d
bini0823 <bini0823@naver.com>
2020-09-26 23:59:21 +0900

    FlatList로 임시 데이터 List 출력

--- src/screens/List.js
+++ src/screens/List.js
@@ -5,8 +5,69 @@ import {
   View,
   Text,
   TouchableOpacity,
+  FlatList,
 } from 'react-native';
 
+const tempData = [
+  {
+    school: ['중앙대학교1'],
+    dates: ['10/1', '10/2'],
+    place: '강남',
+    peopleCount: 4,
+  },
+  {
+    school: ['중앙대학교2'],
+    dates: ['10/1', '10/2'],
+    place: '강남',
+    peopleCount: 4,
+  },
+  {
+    school: ['중앙대학교3'],
+    dates: ['10/1', '10/2'],
+    place: '강남',
+    peopleCount: 4,
+  },
+  {
+    school: ['중앙대학교4'],
+    dates: ['10/1', '10/2'],
+    place: '강남',
+    peopleCount: 4,
+  },
+  {
+    school: ['중앙대학교5'],
+    dates: ['10/1', '10/2'],
+    place: '강남',
+    peopleCount: 4,
+  },
+];
+
 export default function List({navigation}) {
-  return <View></View>;
+  const addBtn = () => {
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('AddMeeting');
+        }}>
+        <Text>Add</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={tempData}
+        ListHeaderComponent={addBtn}
+        renderItem={({item, index}) => (
+          <View>
+            <Text>{item.school}</Text>
+            <Text>{item.peopleCount}</Text>
+            <Text>{item.place}</Text>
+            <Text>{item.dates}</Text>
+          </View>
+        )}
+        keyExtractor={(_item, index) => `${index}`}
+      />
+    </SafeAreaView>
+  );
 }
