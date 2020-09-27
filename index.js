2e0de8aba215bdd05b7ecabd05710cf7ff9963ec
bini0823 <bini0823@naver.com>
2020-09-27 16:45:59 +0900

    MeetingInfo에 관한 action, store, reducer, thunk 함수 구현

--- index.js
+++ index.js
@@ -4,6 +4,19 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import React from 'react';
 import {name as appName} from './app.json';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from './src/modules/index';
+import ReduxThunk from 'redux-thunk';
 
-AppRegistry.registerComponent(appName, () => App);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => Root);
