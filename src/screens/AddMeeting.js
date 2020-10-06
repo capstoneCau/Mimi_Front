ed67a486cb4edd9c8e372089f3a915c12a84af4b
bini0823 <bini0823@naver.com>
2020-10-06 22:40:07 +0900

    install dateTimePicker and test

--- src/screens/AddMeeting.js
+++ src/screens/AddMeeting.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+import {ButtonGroup, Button} from 'react-native-elements';
 import {
   SafeAreaView,
   StyleSheet,
@@ -7,12 +9,40 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
-export default function AddMeeting() {
+export default function AddMeeting({navigation}) {
   return (
     <View>
-      <TouchableOpacity>
-        <Text>AddMeeting</Text>
-      </TouchableOpacity>
+      <DateComp />
+    </View>
+  );
+}
+
+function DateComp() {
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    console.log('print');
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    console.warn('A date has been picked: ', date);
+    hideDatePicker();
+  };
+
+  return (
+    <View>
+      <Button title="Show Date Picker" onPress={showDatePicker} />
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+      />
     </View>
   );
 }
