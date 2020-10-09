d9371b44a1910bd29864b6a69d320f6211f78627
bini0823 <bini0823@naver.com>
2020-10-10 03:33:26 +0900

    react-native paper사용을 위한 초기설정

--- index.js
+++ index.js
@@ -6,17 +6,29 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import React from 'react';
 import {name as appName} from './app.json';
-import {Provider} from 'react-redux';
+import {Provider as StoreProvieder} from 'react-redux';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './src/modules/index';
 import ReduxThunk from 'redux-thunk';
 
 const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    accent: 'yellow',
+  },
+};
+
 const Root = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <StoreProvieder store={store}>
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
+  </StoreProvieder>
 );
 
 AppRegistry.registerComponent(appName, () => Root);
