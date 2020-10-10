fd8ab209c8623b614fc37c120f1a4fa64ef01f5d
whddk4415 <whddk4415@naver.com>
2020-10-10 17:56:20 +0900

    develop branch와 충돌 되는 부분 해결

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -9,25 +9,37 @@ import {
 
 export default function SignUp({navigation}) {
   return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('Home');
-        }}>
-        <Text style={styles.text}>회원가입</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Animated.View
+        style={[
+          styles.textView,
+          {opacity: fadeAnim},
+          startSignUp ? {display: 'none'} : {},
+        ]}>
+        <Text style={styles.text1}>가입을 시작합니다!</Text>
+        <Text style={styles.text2}>
+          카카오계정 하나로 서비스를 이용할 수 있어요!
+        </Text>
+      </Animated.View>
+      {signUp}
+    </SafeAreaView>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: 'red'
+    flexDirection: 'column',
   },
-  text: {
-    height: "100%",
-    width: "100%"
+  textView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  
+  text1: {
+    fontSize: 30,
+    marginBottom: 10,
+  },
+  text2: {
+    fontFamily: 'Geogia',
+  },
 });
