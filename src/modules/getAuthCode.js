e59cc4f2a0a9d73d5f1bb85edfcdadda27132e46
bini0823 <bini0823@naver.com>
2020-10-12 19:25:06 +0900

    정렬

--- src/modules/getAuthCode.js
+++ src/modules/getAuthCode.js
@@ -1,19 +1,19 @@
-const SERVER_DOMAIN = "http://111.118.40.200:8000/mail/"
+const SERVER_DOMAIN = 'http://111.118.40.200:8000/mail/';
 
 export const getAuthCode = async (address) => {
-    const res = await fetch(SERVER_DOMAIN, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, cors, *same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-            'Content-Type': 'application/json',
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrer: 'no-referrer', // no-referrer, *client
-        body: JSON.stringify({"address": address}), // body data type must match "Content-Type" header
-    })
-    const token = await res.json()
-    return token.token
-}
+  const res = await fetch(SERVER_DOMAIN, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, cors, *same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrer: 'no-referrer', // no-referrer, *client
+    body: JSON.stringify({address: address}), // body data type must match "Content-Type" header
+  });
+  const token = await res.json();
+  return token.token;
+};
