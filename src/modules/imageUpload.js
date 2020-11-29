b25ea18c534ba3386aaf5f12dc9dc65b02479385
whddk4415 <whddk4415@naver.com>
2020-11-30 00:15:32 +0900

    동물 카테고리 선택 시 이미지 받아오는 module 추가

--- src/modules/imageUpload.js
+++ src/modules/imageUpload.js
@@ -3,7 +3,7 @@ import RNFetchBlob from 'rn-fetch-blob';
 
 export const uploadImage = async (uri, gender) => {
   const data = await RNFetchBlob.fs.readFile(uri, 'base64');
-  const res = await fetch(SERVER_DOMAIN + 'image/', {
+  const res = await fetch(SERVER_DOMAIN + 'image/detect/', {
     method: 'POST',
     mode: 'cors',
     headers: {
@@ -15,6 +15,26 @@ export const uploadImage = async (uri, gender) => {
     }),
   });
   const json = await res.json();
+  return json;
+};
+
+export const selectAnimalLabel = async (label) => {
+  const res = await fetch(SERVER_DOMAIN + `image/getImage?label=${label}`, {
+    method: 'GET',
+    mode: 'cors',
+  });
+
+  const json = await res.json();
+  const images = [];
+  // json.images.forEach((val, idx) => {
+  //   images.push({
+  //     id: val.id,
+  //     image: 'data:image/jpeg;base64,' + val.base64,
+  //   });
+  //   console.log(images[idx].image.substring(0, 50));
+  // });
+  // const data = await RNFetchBlob.fs.readFile(uri, 'base64');
+  // console.log(json);
   return json;
 };
 
