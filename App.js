b6e594ae04110a8e2780df9a9006c0e8def4f1c2
bini0823 <bini0823@cau.ac.kr>
2021-02-14 01:47:37 +0900

    강제 SignUp Page 이동

--- App.js
+++ App.js
@@ -202,7 +202,7 @@ const App = () => {
   const Navigator = () => {
     const loginStack = () => {
       return (
-        <Stack.Navigator initialRouteName="Login">
+        <Stack.Navigator initialRouteName="SignUp">
           <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="SignUp" component={SignUp} />
         </Stack.Navigator>
@@ -345,14 +345,16 @@ const App = () => {
 
     return (
       <NavigationContainer theme={MyTheme}>
-        <Stack.Navigator initialRouteName={initDestination} headerMode="false">
+        {/* <Stack.Navigator initialRouteName={initDestination} headerMode="false">
+         */}
+        <Stack.Navigator initialRouteName="Login" headerMode="false">
           <Stack.Screen name="Login" component={loginStack} />
           <Stack.Screen name="Home" component={homeStack} />
         </Stack.Navigator>
       </NavigationContainer>
     );
   };
-  if (initializing) return null;
+  // if (initializing) return null;
   return <Navigator />;
 };
 
